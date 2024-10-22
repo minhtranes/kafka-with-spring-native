081fea076104e549fdec00d4af8af95e242b5b31
Minh Trần <wisekingtran@gmail.com>
2024-10-22 17:33:55 +0700

    Initialize the project

---
+++ src/main/java/vn/ss/study/KafkaWithSpringNativeApplication.java
@@ -0,0 +1,13 @@
+package vn.ss.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaWithSpringNativeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaWithSpringNativeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/ss/study/KafkaWithSpringNativeApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.ss.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaWithSpringNativeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
